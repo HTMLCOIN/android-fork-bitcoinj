21895fbd3672f457f736ffb5052458b594bec38e
Vu Do <dohaivu@gmail.com>
2018-01-15 16:48:02 +0700

    change addressHeader, p2shHeader, dns, checkpoint

--- core/src/main/java/org/bitcoinj/params/HtmlCoinMainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/HtmlCoinMainNetParams.java
@@ -4,6 +4,7 @@ import org.bitcoinj.core.ECKey;
 import org.bitcoinj.core.Sha256Hash;
 import org.bitcoinj.core.Utils;
 import org.bitcoinj.net.discovery.HttpDiscovery;
+import org.bitcoinj.params.AbstractBitcoinNetParams;
 
 import java.net.URI;
 
@@ -20,9 +21,9 @@ public class HtmlCoinMainNetParams extends AbstractBitcoinNetParams {
         interval = INTERVAL;
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
-        dumpedPrivateKeyHeader = 128;
-        addressHeader = 58;
-        p2shHeader = 50;
+        dumpedPrivateKeyHeader = 169;
+        addressHeader = 41; // chainparams.cpp, line 173
+        p2shHeader = 100;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 8333;
         packetMagic = 0xf9beb4d9L;
@@ -47,20 +48,12 @@ public class HtmlCoinMainNetParams extends AbstractBitcoinNetParams {
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(91722, Sha256Hash.wrap("00000000000271a2dc26e7667f8419f2e15416dc6955e5a6c6cdf3f2574dd08e"));
-        checkpoints.put(91812, Sha256Hash.wrap("00000000000af0aed4792b1acee3d966af36cf5def14935db8de83d6f9306f2f"));
-        checkpoints.put(91842, Sha256Hash.wrap("00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec"));
-        checkpoints.put(91880, Sha256Hash.wrap("00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721"));
-        checkpoints.put(200000, Sha256Hash.wrap("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf"));
+        checkpoints.put(0, Sha256Hash.wrap("0000bf23c6424c270a24a17a3db723361c349e0f966d7b55a6bca4bfb2d951b0"));
+        checkpoints.put(798, Sha256Hash.wrap("00002847d05b6fe46570b754815309123bedcb84a5ac2ae58fa1d38957ccb772"));
 
         dnsSeeds = new String[] {
-                "seed.bitcoin.sipa.be",         // Pieter Wuille
-                "dnsseed.bluematt.me",          // Matt Corallo
-                "dnsseed.bitcoin.dashjr.org",   // Luke Dashjr
-                "seed.bitcoinstats.com",        // Chris Decker
-                "seed.bitnodes.io",             // Addy Yeow
-                "bitseed.xf2.org",              // Jeff Garzik
-                "seed.bitcoin.jonasschnelli.ch" // Jonas Schnelli
+                "seed1.htmlcoin.com",
+                "seed2.htmlcoin.com"
         };
         httpSeeds = new HttpDiscovery.Details[] {
                 // Andreas Schildbach
